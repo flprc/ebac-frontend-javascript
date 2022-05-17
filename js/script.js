8ecfa4c51bbb9ca7a9be370bbe81316d5ecf00a0
Felipe Cavalcante <felipecavalcante.c@hotmail.com>
2022-05-17 03:08:45 -0300

    modulo-13

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function obterMedia(){
+
+  var a = 10;
+  var b = 20;
+  var c = 30;
+  
+  var media = (a + b + c) / 3;
+  
+  document.getElementById("resultado").innerHTML = "Resultado: " + media;
+
+}
+
+function obterMedia2(){
+
+    var a = 20;
+    var b = 30;
+    var c = 40;
+    
+    var media = (a + b + c) / 3;
+    
+    document.getElementById("resultado").innerHTML = "Resultado: " + media;
+  
+  }
+
+  function obterMedia3(){
+
+    var a = 45;
+    var b = 65;
+    var c = 25;
+    
+    var media = (a + b + c) / 3;
+    
+    document.getElementById("resultado").innerHTML = "Resultado: " + media;
+  
+  }
